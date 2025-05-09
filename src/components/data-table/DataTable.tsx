065ed4fbb4b67a0c8681617dd7339a7439f96df8
Wesly Michel <michel.wesly@gmail.com>
2025-05-09 13:01:25 -0400

    Update DT - rm React import

--- src/components/data-table/DataTable.tsx
+++ src/components/data-table/DataTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import  { useState, useMemo } from 'react';
 import type { DataTableProps, Column, SortState } from './DataTable.types';
 import './DataTable.css';
 
